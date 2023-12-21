b75e8a3c3d989b02d8727fa730a4362cc93789a3
MiguelR <miguelrosendo29@gmail.com>
2023-12-21 01:44:15 +0000

    now has a card ready to recieve data

--- Planet-View/js/app/views/planet-view.js
+++ Planet-View/js/app/views/planet-view.js
@@ -2,9 +2,38 @@ define(function () {
     var internals = {
         handlers: {},
         elements: {},
+        data: {},
     };
 
     var externals = {};
+
+    internals.createCard = function () {
+        const card = `<div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+          <p>Planet: ${internals.data.planet.name}</p>
+          <p>Type: ${internals.data.planet.type}</p>
+          <p>Gravity: ${internals.data.planet.gravity}</p>
+          <p>Average surface temperature: ${internals.data.planet.temperature}</p>
+          <p>Diameter: ${internals.data.planet.Diameter}</p>
+          <p>Satellites: ${internals.data.planet.satellites} Names: ${internals.data.planet.satellites[0]}, ${internals.data.planet.satellites[1]}</p>
+          <p>Distance to the Sun: ${internals.data.planet.sunDistance}</p>
+          <p>Light travel time: ${internals.data.planet.oneWayLightToTheSun}</p>
+          </div>
+          <div class="flip-card-back">
+            <h1>${internals.data.sailors.scoutName}</h1> 
+            <p>Name: ${internals.data.sailors.englishName}/${internals.data.sailors.japaneseName}</p>
+            <p>Description : ${internals.data.sailors.description}</p>
+            <p>Birthday : ${internals.data.sailors.birthday}</p>
+            <p>Transformation phrase : ${internals.data.sailors.transformation_phrase}</p>
+            <p>Powers : ${internals.data.sailors.powers}</p>
+            <p>Likes : ${internals.data.sailors.likes}</p>
+            <p>Dislikes : ${internals.data.sailors.dislikes}</p>
+          </div>
+        </div>
+      </div>`;
+        return card;
+    };
 
     internals.createButton = function () {
         const backToMenu = `<div class="backToMenu"><button id="backToMenu">Solar System</button></div>`;
@@ -22,7 +51,7 @@ define(function () {
     }
 
     internals.createMainDiv = function() {
-        //const backToMenu = internals.createButton();
+        //const card = internals.createCard();
         const planetDiv = internals.createPlanetDiv();
         const sailor = internals.createSailor();
           
@@ -52,7 +81,10 @@ define(function () {
         console.log("Button is rendered");
     }
 
-    externals.render = function () {
+    externals.render = function (sailor, planet) {
+
+        internals.data.sailor = sailor;
+        internals.data.planet = planet;
         internals.elements.app = $('body');
         internals.renderButton();
         internals.renderMainDiv();
